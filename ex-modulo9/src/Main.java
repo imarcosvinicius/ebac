da168bd90b0533b1bf53ee92ae5373c00d3d17e2
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-06-08 21:36:01 -0300

    comentarios em codigo

--- ex-modulo9/src/Main.java
+++ ex-modulo9/src/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input do tipo primitivo
         int idade = sc.nextInt();
 
+        // Casting
         Integer novaIdade = idade;
 
+        // Print em Wrapper
         System.out.println(novaIdade);
 
     }
