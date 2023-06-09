29f0bced2509bda8aca23a751de08c8cce7e6ec1
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-06-08 21:32:17 -0300

    exercicio modulo 9

---
+++ ex-modulo9/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int idade = sc.nextInt();
+
+        Integer novaIdade = idade;
+
+        System.out.println(novaIdade);
+
+    }
+
+}
